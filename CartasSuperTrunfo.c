d5c9f359c57eaf641994b62a2619b2ef9112c94a
NayaraFont <nayarafm24@gmail.com>
2025-02-23 05:13:48 +0000

    Atualizei o Jogo Cartas Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -50,6 +50,29 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontos_turisticos2); 
     
+    printf("\nComparação de cartas (Atributo: População):\n");
+    printf("Carta 1 - %s (%c): %d\n", cidade1, estado1, populacao1);
+    printf("Carta 2 - %s (%c): %d\n", cidade2, estado2, populacao2);
+    if (populacao1 > populacao2) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", cidade1);
+    } else if (populacao2 > populacao1) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", cidade2);
+    } else {
+        printf("Resultado: Empate!\n");
+    }
 
+    float densidade1 = populacao1 / area1;
+    float densidade2 = populacao2 / area2;
+    printf("\nComparação de cartas (Atributo: Densidade Populacional):\n");
+    printf("Carta 1 - %s (%c): %.2f hab/km²\n", cidade1, estado1, densidade1);
+    printf("Carta 2 - %s (%c): %.2f hab/km²\n", cidade2, estado2, densidade2);
+    if (densidade1 < densidade2) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", cidade1);
+    } else if (densidade2 < densidade1) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", cidade2);
+    } else {
+        printf("Resultado: Empate!\n");
+    }
+    
     return 0;
 }
